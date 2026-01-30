03be5369d4ec792dc5e4ff22b08834d88b63e34d
Zonkodonko <zonkolp.123@gmail.com>
2026-01-30 14:03:19 +0100

    Add name constants

---
+++ src/data/NameConstants.ts
@@ -0,0 +1,22 @@
+//Teams
+export const BLUE = 'Order'
+export const RED = 'Chaos'
+
+//Structures
+export const INHIB = 'Inhib'
+export const TURRET = 'Turret'
+
+//Lanes
+export const LANE_BOT = 'L0'
+export const LANE_MID = 'L1'
+export const LANE_TOP = 'L2'
+
+//Turret positions
+export const T_POSITION_OUT = 'P3'
+export const T_POSITION_BASE = 'P2'
+export const T_POSITION_INHIB = 'P1'
+
+
+
+//Turret proxy is = Turret_T{team}_{lane}_{position}_{weird random number}
+//Inhib proxy is = Inhib_T{team}_{lane}_{position}_{weird random number}
