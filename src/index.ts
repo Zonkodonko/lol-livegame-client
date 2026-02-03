8e124dca2015a6009cb26018535eddbb1336baeb
Zonkodonko <zonkolp.123@gmail.com>
2026-02-03 19:42:04 +0100

    optimized project settings for publishing

---
+++ src/index.ts
@@ -0,0 +1,28 @@
+export * from './data/Turret.js'
+export * from './data/Player.js'
+export * from './data/ActivePlayer.js'
+export * from './api/LiveApiClient.js'
+export * from './api/events/EventManager.js'
+
+export * from './data/LoLEvents.js'
+export * from './data/Constants.js'
+export * from './data/Maps.js'
+
+export * from './data/LiveData.js'
+export * from './data/GameData.js'
+
+export * from './data/Player.js'
+export * from './data/ActivePlayer.js'
+export * from './data/ChampionAbilities.js'
+export * from './data/ChampionStats.js'
+export * from './data/SummonerSpells.js'
+
+export * from './data/Item.js'
+
+export * from './data/runes/PlayerRunes.js'
+export * from './data/runes/FullRunes.js'
+export * from './data/runes/Rune.js'
+export * from './data/runes/RuneTree.js'
+export * from './data/runes/StatRune.js'
+
+
